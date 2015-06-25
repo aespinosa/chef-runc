b1f8c56bd41126cfd1db47aa6327a5f953aa0c98
Allan Espinosa <allan.espinosa@outlook.com>
2015-06-26 08:20:41 +0900

    Create the cookbook skeleton

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'runc'
+maintainer       'Allan Espinosa'
+maintainer_email 'allan.espinosa@outlook.com'
+license          'Apache 2.0'
+description      'Installs/Configures runc'
+long_description 'Installs/Configures runc'
+version          '0.0.0'
